56a8ac42f55aa04cab3d275f7b53eeace61bdc66
Lukáš Hlubuček <lukas.hlubucek@firma.seznam.cz>
2016-03-20 22:44:48 +0100

    Parse data from web add link into index site
    ToDo:
    + insert into database

--- tradice-dump.php
+++ tradice-dump.php
@@ -1,12 +1,17 @@
 <?php
+
 class Day {
-	public $day;
-	public $foods = [];
+
+    public $day;
+    public $foods = [];
+
 }
 
 class Food {
-	public $name;
-	public $price;
+
+    public $name;
+    public $price;
+
 }
 
 header('Content-Type: text/html; charset=utf-8');
@@ -19,9 +24,7 @@ $port = 8889;
 $table = 'table_foods';
 $endPoint = 'http://www.tradiceandel.cz/cz/denni-nabidka/';
 $link = mysql_connect(
-   "$host:$port",
-   $user,
-   $password
+        "$host:$port", $user, $password
 );
 
 $curl_init = curl_init($endPoint);
@@ -36,13 +39,35 @@ $classname = 'separator-section';
 $xpath = new DOMXPath($dom);
 $results = $xpath->query("//*[@class='" . $classname . "']");
 
-for ($index = 0; $index < 1; $index++) {
+$parsedFoods = [];
+$days = array("po", "út", "st", "čt", "pa");
+
+for ($index = 0; $index < $results->length; $index++) {
     $item0 = $results->item($index);
     $foods = $item0->childNodes;
+    $day = new Day();
+    $day->day = $days[$index];
     for ($index1 = 0; $index1 < $foods->length; $index1++) {
+        $counter = 0;
+        $food = new Food();
         $hnuj = $foods->item($index1)->childNodes;
-        foreach ($hnuj as $bubu) {
-            var_dump($bubu);
+        if ($hnuj instanceof DOMNodeList) {
+            foreach ($hnuj as $foo) {
+                if ($foo instanceof DOMElement) {
+                    $value = preg_replace("/ {2,}/", " ", $foo->nodeValue);
+                    $value = preg_replace("/\n/", "", $value);
+                    if ($counter % 2 == 0) {
+                        $food->name = $value;
+                    } else {
+                        $food->price = $value;
+                        $day->foods[] = $food;
+                    }
+
+                    $counter++;
+                }
+            }
         }
     }
+    $parsedFoods[] = $day;
 }
+echo json_encode($parsedFoods);
